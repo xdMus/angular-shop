e503a7a5b7688099be6e148d0df6ca16ec77ec94
Dmitrii Sysoev <dmitrii_sysoev1@epam.com>
2023-07-07 22:52:02 +0400

    configure the project

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "*.{ts, html}": [
+  "eslint --fix"
+  ],
+  "*.{json,css,scss,less,md,ts,html}": [
+  "prettier --write"
+  ]
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  bracketSpacing: true,
+  trailingComma: "es5",
+  bracketSameLine: true,
+  printWidth: 100
+}
